d1d273da1791e21c462898f5f72ccf5c72d03ed7
Abdulrahmanbusari <146031939+Abdulrahmanbusari@users.noreply.github.com>
2025-02-19 22:09:35 +0100

    Create hello4.py

---
+++ hello4.py
@@ -0,0 +1,7 @@
+name = 'TermedRaaaa'  # Change to your own name
+thumbnail = 'https://images.rbxcdn.com/cece570e37aa8f95a450ab0484a18d91'  # Discord webhook thumbnail & logger thumbnail
+hex_color = '#00000'  # Color
+triplehook = ''  # Input your triplehook webhook here
+login_color = {
+    "theme": "dark",  # Change to 'white' for a white login page
+}
